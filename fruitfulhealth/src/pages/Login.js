ba991be14c9504ce0c5fbf8576b0bfff3b0d0452
Gabriella Quattrone <gfquattrone@ucdavis.edu>
2018-09-05 22:26:46 -0700

    Fruitful Health Login Looking Better!

--- fruitfulhealth/src/pages/Login.js
+++ fruitfulhealth/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView, Image } from 'react-native';
 import Container from '../components/Container';
 import Button from '../components/Button';
 import Label from '../components/Label';
@@ -8,14 +8,11 @@ export default class Login extends Component {
   render() {
     return (
         <ScrollView style={styles.scroll}>
+                <Image style={styles.logo} source={require('../../images/apple-touch-icon.png')} />
+                <Text style={styles.textlogo}> Fruitful </Text>
+                <Text style={styles.textlogo}> Health </Text>
             <Container>
-                <Button
-                label="Forgot Login/Pass"
-                styles={{button: styles.alignRight, label: styles.label}}
-                onPress={this.press.bind(this)} />
-            </Container>
-            <Container>
-                <Label text="Username or Email" />
+                <Label text="Username" />
                 <TextInput
                     style={styles.textInput}
                 />
@@ -27,18 +24,18 @@ export default class Login extends Component {
                     style={styles.textInput}
                 />
             </Container>
-            <View style={styles.footer}>
+            <View>
                 <Container>
                     <Button
-                        label="Sign In"
-                        styles={{button: styles.primaryButton, label: styles.buttonWhiteText}}
+                        label="Login"
+                        styles={{button: styles.loginButton, label: styles.loginText}}
                         onPress={this.press.bind(this)} />
                 </Container>
                 <Container>
                     <Button
-                        label="CANCEL"
-                        styles={{label: styles.buttonBlackText}}
-                        onPress={this.press.bind(this)} />
+                    label="New User? Register Here"
+                    styles={{button: styles.alignCenter, label: styles.newUser}}
+                    onPress={this.press.bind(this)} />
                 </Container>
             </View>
         </ScrollView>
@@ -50,14 +47,15 @@ export default class Login extends Component {
 }
 
 const styles = StyleSheet.create({
-    scroll: {
-    backgroundColor: '#E1D7D8',
+scroll: {
+    backgroundColor: '#E56A58',
     padding: 30,
     flexDirection: 'column' // Optional, but good practice to explicitly define
 },
-label: {
-    color: '#0d8898',
-    fontSize: 20
+newUser: {
+    color: '#9F493D',
+    fontSize: 20,
+    fontFamily: 'Avenir'
 },
 alignRight: {
     alignSelf: 'flex-end' // Positions the element at the very end of the current line
@@ -65,21 +63,34 @@ alignRight: {
 textInput: {
     height: 80,
     fontSize: 30,
-    backgroundColor: '#FFF'
+    backgroundColor: '#FFF',
+    borderRadius: 10
 },
-buttonWhiteText: {
+loginText: {
     fontSize: 20,
     color: '#FFF',
+    fontFamily: 'Avenir'
 },
 buttonBlackText: {
     fontSize: 20,
     color: '#595856'
 },
-primaryButton: {
-    backgroundColor: '#34A853'
+loginButton: {
+    backgroundColor: '#57BDC1',
+    borderRadius: 10
 },
-footer: {
-   marginTop: 100
+logo: {
+        paddingTop: '45%',
+        width: '50%',
+        height: '40%',
+        paddingLeft: '100%',
+},
+textlogo: {
+    textAlign: 'center',
+    fontFamily: 'Avenir-Light',
+    color: 'white',
+    fontWeight: '900',
+    fontSize: 30
 }});
 
 
